3bc84495ceb0798b1b98bccfe8dc0c508990d889
Hvuu36 <hmvu157@gmail.com>
2026-01-17 20:51:33 +0700

    Demo part1

---
+++ lmsww/Assets/Script/showlog.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class showlog : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update called! " + Time.frameCount);
+    }
+
+}
